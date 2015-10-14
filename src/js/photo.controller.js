39777c5b6d934330a5298af5debf3676e9672d9c
arydman <duck4321>
2015-10-14 15:00:24 +0200

    Nicer Visual Feedback from GUI elements. Implented fallbacks for camera/datepicker in non-cordova (ie, node-js devserver) environments.

--- src/js/photo.controller.js
+++ src/js/photo.controller.js
@@ -7,14 +7,31 @@
 
     function PhotoController($scope, Camera, $state, $cordovaDatePicker, $window, reminderService, $ionicLoading, upload) {
 
+
+        $scope.activate = function () {
+            $scope.formData = {
+                title: 'Photo',
+                date: undefined
+            };
+            $scope.picFile = undefined;
+
+            $scope.photoUri = undefined;
+        }
+
         $scope.getPhoto = function () {
-            Camera.getPicture().then(function (imageURI) {
-                $scope.photoUri = imageURI
-                $scope.getDatePicker();
-            }, function (err) {
-                console.err(err);
-                $state.go('app.reminders');
-            });
+
+            if ($window.cordova) {
+                Camera.getPicture().then(function (imageURI) {
+                    $scope.photoUri = imageURI
+                    $scope.getDatePicker();
+                }, function (err) {
+                    console.err(err);
+                    $state.go('app.reminders');
+                });
+            }
+            else {
+                alert('no cordova, please use upload button instead');
+            }
         };
 
         $scope.fileUpdated = function ($files, $event) {
@@ -25,35 +42,50 @@
 
         $scope.getDatePicker = function () {
             //messy  
-            var options = {
-                date: new Date(),
-                mode: 'time', // or 'time' 
-                minDate: new Date() - 10000,
-                allowOldDates: false,
-                allowFutureDates: true,
-                doneButtonLabel: 'DONE',
-                doneButtonColor: '#F2F3F4',
-                cancelButtonLabel: 'CANCEL',
-                cancelButtonColor: '#000000'
-            };
+            console.log('getDatePicker called');
 
-            document.addEventListener("deviceready", function () {
-                $cordovaDatePicker.show().then(function (date) {
-                    $scope.reminderDate = date;
-                });
-            }, false);
+            if ($window.cordova) {
+                var options = {
+                    date: new Date(),
+                    mode: 'time', // or 'time' 
+                    minDate: new Date() - 10000,
+                    allowOldDates: false,
+                    allowFutureDates: true,
+                    doneButtonLabel: 'DONE',
+                    doneButtonColor: '#F2F3F4',
+                    cancelButtonLabel: 'CANCEL',
+                    cancelButtonColor: '#000000'
+                };
+                
+                // never triggered in web. 
+                document.addEventListener("deviceready", function () {
+                    $cordovaDatePicker.show().then(function (date) {
+                        $scope.reminderDate = date;
+                        $scope.formData.date = date;
+                    });
+                }, false);
+            }
+
+            else {
+                $scope.noCordovaDate = true;
+                $scope.formData.date = new Date();
+            }
         };
 
         $scope.createReminder = function () {
             $ionicLoading.show();
-            reminderService.createReminder($scope.title, new Date())
+            reminderService.createReminder($scope.formData.title, $scope.formData.date)
                 .then(function (response) {
                     if (response.success) {
                         /* Created!, Lets upload the file */
                         reminderService.uploadImage(response.reminder.id, $scope.picFile).then(function (response) {
+                            $ionicLoading.hide();
                             $state.go('app.reminders');
-                        }); 
-                        $ionicLoading.hide(); 
+
+                            // reinit. 
+                            $scope.activate();
+                        });
+
                     }
                 },
                     function (err) {
@@ -65,6 +97,8 @@
         document.addEventListener("deviceready", function () {
             $scope.getPhoto();
         }, false);
+
+        $scope.activate();
 
     }
 } ());
--- www/js/app.dist.js
+++ www/js/app.dist.js
@@ -87,14 +87,31 @@ angular.module('pr', ['ionic', 'ngCordova', 'ngFileUpload'])
 
     function PhotoController($scope, Camera, $state, $cordovaDatePicker, $window, reminderService, $ionicLoading, upload) {
 
+
+        $scope.activate = function () {
+            $scope.formData = {
+                title: 'Photo',
+                date: undefined
+            };
+            $scope.picFile = undefined;
+
+            $scope.photoUri = undefined;
+        }
+
         $scope.getPhoto = function () {
-            Camera.getPicture().then(function (imageURI) {
-                $scope.photoUri = imageURI
-                $scope.getDatePicker();
-            }, function (err) {
-                console.err(err);
-                $state.go('app.reminders');
-            });
+
+            if ($window.cordova) {
+                Camera.getPicture().then(function (imageURI) {
+                    $scope.photoUri = imageURI
+                    $scope.getDatePicker();
+                }, function (err) {
+                    console.err(err);
+                    $state.go('app.reminders');
+                });
+            }
+            else {
+                alert('no cordova, please use upload button instead');
+            }
         };
 
         $scope.fileUpdated = function ($files, $event) {
@@ -105,35 +122,50 @@ angular.module('pr', ['ionic', 'ngCordova', 'ngFileUpload'])
 
         $scope.getDatePicker = function () {
             //messy  
-            var options = {
-                date: new Date(),
-                mode: 'time', // or 'time' 
-                minDate: new Date() - 10000,
-                allowOldDates: false,
-                allowFutureDates: true,
-                doneButtonLabel: 'DONE',
-                doneButtonColor: '#F2F3F4',
-                cancelButtonLabel: 'CANCEL',
-                cancelButtonColor: '#000000'
-            };
+            console.log('getDatePicker called');
 
-            document.addEventListener("deviceready", function () {
-                $cordovaDatePicker.show().then(function (date) {
-                    $scope.reminderDate = date;
-                });
-            }, false);
+            if ($window.cordova) {
+                var options = {
+                    date: new Date(),
+                    mode: 'time', // or 'time' 
+                    minDate: new Date() - 10000,
+                    allowOldDates: false,
+                    allowFutureDates: true,
+                    doneButtonLabel: 'DONE',
+                    doneButtonColor: '#F2F3F4',
+                    cancelButtonLabel: 'CANCEL',
+                    cancelButtonColor: '#000000'
+                };
+                
+                // never triggered in web. 
+                document.addEventListener("deviceready", function () {
+                    $cordovaDatePicker.show().then(function (date) {
+                        $scope.reminderDate = date;
+                        $scope.formData.date = date;
+                    });
+                }, false);
+            }
+
+            else {
+                $scope.noCordovaDate = true;
+                $scope.formData.date = new Date();
+            }
         };
 
         $scope.createReminder = function () {
             $ionicLoading.show();
-            reminderService.createReminder($scope.title, new Date())
+            reminderService.createReminder($scope.formData.title, $scope.formData.date)
                 .then(function (response) {
                     if (response.success) {
                         /* Created!, Lets upload the file */
                         reminderService.uploadImage(response.reminder.id, $scope.picFile).then(function (response) {
+                            $ionicLoading.hide();
                             $state.go('app.reminders');
-                        }); 
-                        $ionicLoading.hide(); 
+
+                            // reinit. 
+                            $scope.activate();
+                        });
+
                     }
                 },
                     function (err) {
@@ -145,6 +177,8 @@ angular.module('pr', ['ionic', 'ngCordova', 'ngFileUpload'])
         document.addEventListener("deviceready", function () {
             $scope.getPhoto();
         }, false);
+
+        $scope.activate();
 
     }
 } ());
